8412914b9e165cdc6a9a716a2ce3dde27a5468aa
Santa <s.yushmanov777@gmail.com>
2018-05-03 20:32:15 +0700

    create header block, create working-process mixin

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+/*const but = document.querySelector(".header__menu-but");
+const menu = document.querySelector(".header__menu");
+
+but.addEventListener("click", e => {
+  e.preventDefault();
+  menu.style.display = "block";
+});
+
+*/
