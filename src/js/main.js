35b01f65012f3fec41499789504cee0bcf9ed13a
Santa <s.yushmanov777@gmail.com>
2018-05-04 03:28:47 +0700

    create working-process block, add mixin, add font-awasome

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
-/*const but = document.querySelector(".header__menu-but");
+const but = document.querySelector(".header__menu-but");
 const menu = document.querySelector(".header__menu");
 
 but.addEventListener("click", e => {
   e.preventDefault();
-  menu.style.display = "block";
+  menu.classList.toggle('active')
 });
 
-*/
+
